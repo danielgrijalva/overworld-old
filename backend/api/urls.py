50672f277a9d4edf6f19c341aaa4226ca8bf8b5e
daniel grijalva <jdgs.gt@gmail.com>
2019-04-20 21:19:23 -0700

    :construction: log and search endpoints

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('games/<int:igdb>/', views.get_game)
+    path('games/<int:igdb>/', views.get_game),
+    path('log/', views.log),
+    path('search/<str:name>', views.search)
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,35 @@
-import json
 import requests
 from django.conf import settings
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import GameSerializer
 
 
 @api_view()
 def get_game(request, igdb):
     data = f'fields *; where id={igdb};'
     headers={'user-key': settings.IGDB_KEY}
-    r = requests.post(settings.IGDB_URL, data=data, headers=headers)
+    url = settings.IGDB_URL.format(endpoint='games')
+    r = requests.post(url=url, data=data, headers=headers)
 
-    return Response(json.loads(r.content))
+    return Response(r.json())
     
+@api_view(['GET', 'POST'])
+def log(request):
+    if request.method == 'POST':
+        serializer = GameSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def search(request, name):
+    params = {'search': name, 'fields': 'name'}
+    headers={'user-key': settings.IGDB_KEY}
+    url = settings.IGDB_URL.format(endpoint='games')
+    r = requests.post(url=url, params=params, headers=headers)
+
+    return Response(r.json())
