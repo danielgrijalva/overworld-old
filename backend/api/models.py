f5f764802ce2f3434be52931999d095b1b9e467a
daniel grijalva <jdgs.gt@gmail.com>
2019-04-20 21:14:48 -0700

    :card_file_box: update models - `name` field

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
 class Game(models.Model):
     igdb = models.IntegerField()
+    name = models.CharField(max_length=255, default='default')
     
