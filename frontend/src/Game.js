348310d964daf5e8a09a8177ff6283a294d54fe4
Daniel Grijalva <jdgs.gt@gmail.com>
2019-04-23 16:02:17 -0700

    :construction: game details

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -1,90 +1,144 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Grid, Image, Loader, Menu, Icon } from "semantic-ui-react";
+
 import axios from "axios";
 import Moment from "react-moment";
+import './Game.css'
 
 class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      gameId: '',
       game: {},
-      isLoading: false,
-      cover: ""
+      isLoading: true,
+      isCoverLoading: true,
+      isCompanyLoading: true,
+      cover: {},
+      company: ''
     };
-
-    this.loadGame = this.loadGame.bind(this);
-    this.getCover = this.getCover.bind(this);
-    this.getCompany = this.getCompany.bind(this);
   }
 
   componentWillReceiveProps(props) {
-    this.loadGame(props.location.state);
-  }
-
-  componentWillMount() {
-    this.loadGame(this.props.location.state);
-  }
-
-  loadGame(game) {
-    this.setState({ isLoading: true });
-
-    if (game) {
-      this.getCover(game.cover);
-      this.getCompany(game.involved_companies[0]);
-      this.setState({ game: game, isLoading: false });
-    } else {
-      // user entered the page directly
-      // should fetch the game by reading the slug
-      // and then calling the Search API
-      console.log("fetching...");
+    if (props.location.state !== this.state.gameId) {
+      const gameId = props.location.state;
+      this.resetState(gameId);
+      this.loadGame(gameId);
     }
   }
 
-  getCover(id) {
-    axios
-      .get(`/api/covers/${id}`)
-      .then(response => {
-        this.setState({ cover: response.data[0].image_id });
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+  componentWillMount() {
+    const gameId = this.props.location.state;
+    this.resetState(gameId);
+
+    this.loadGame(gameId);
   }
 
-  getCompany(id) {
-    axios
-      .get(`/api/companies/${id}`)
+  resetState = gameId => {
+    this.setState({
+      gameId: gameId,
+      game: {},
+      isLoading: true,
+      isCoverLoading: true,
+      isCompanyLoading: true,
+      cover: {},
+      company: ''
+    })
+  }
+
+  loadGame = gameId => {
+    axios.get(`/api/games/${gameId}`)
       .then(response => {
-        this.setState({ company: response.data[0].name });
+        const game = response.data[0];
+        this.setState({ isLoading: false, game: game })
+        this.getGameData(game)
       })
-      .catch(function(error) {
-        console.log(error);
-      });
+  }
+
+  getGameData = game => {
+    axios.all([this.getCover(game.cover), this.getDeveloper(game.id)])
+      .then(axios.spread((cover, company) => {
+        this.setState({
+          isCompanyLoading: false,
+          isCoverLoading: false,
+          cover: {
+            cover: cover.data[0].image_id,
+            width: cover.data[0].width,
+            height: cover.data[0].height,
+          },
+          company: company.data[0]
+        })
+      }))
+  }
+
+  getCover = id => {
+    return axios.get(`/api/covers/${id}`)
+  }
+
+
+  getDeveloper = gameId => {
+    return axios.get(`/api/developers/${gameId}`)
   }
 
   render() {
-    const { game, isLoading, cover, company } = this.state;
+    const {
+      game,
+      isLoading,
+      isCoverLoading,
+      isCompanyLoading,
+      cover,
+      company } = this.state;
+
+    if (isLoading) {
+      return <p>loading</p>
+    }
 
     return (
       <Container>
-        <p>{game.name}</p>
-        <p>{game.summary}</p>
-        <div>
-          <Moment unix format="DD/MM/YYYY">
-            {game.first_release_date}
-          </Moment>
-          {!isLoading ? (
-            <React.Fragment>
-              <p>{company}</p>
-              <img
-                alt="cover"
-                src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${cover}.jpg`}
-              />
-            </React.Fragment>
-          ) : (
-            <p>loading image</p>
-          )}
-        </div>
+        <Grid verticalAlign={!isCoverLoading ? 'top' : 'middle'} centered>
+          <Grid.Row>
+            <Grid.Column width={4}>
+              {!isCoverLoading ?
+                <Image
+                  rounded
+                  className="cover frame"
+                  src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${cover.cover}.jpg`}
+                />
+
+                :
+                <Loader active inline="centered" size="medium" />
+              }
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <section className="game-header margin-bottom-sm">
+                <h1>{game.name}</h1>
+                <small className="release-date">
+                  <a href="#"><Moment unix format="YYYY">
+                    {game.first_release_date}
+                  </Moment></a>
+                </small>
+
+              </section>
+              <section className="game-info margin-bottom-sm">
+                {!isCompanyLoading ?
+                  <small className="company">A game by&nbsp;
+
+                    <a href="#">{company.name}</a>
+                  </small>
+
+                  :
+                  null
+                }
+              </section>
+              <section>
+                <p>{game.summary}</p>
+              </section>
+            </Grid.Column>
+            <Grid.Column width={4}>
+                n
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Container>
     );
   }
