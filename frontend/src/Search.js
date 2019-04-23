b69d9a33f55ff28156feb2994b84c58d7012dbdd
Daniel Grijalva <jdgs.gt@gmail.com>
2019-04-23 16:01:39 -0700

    :recycle: passing id only

--- frontend/src/Search.js
+++ frontend/src/Search.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { debounce } from "lodash";
 import { Search, Grid, Label } from "semantic-ui-react";
 
-const resultRenderer = ({ id, name, title }) => (
+const resultRenderer = ({ id, name }) => (
   <Label key={id} content={name} />
 );
 
@@ -23,7 +23,7 @@ class GameSearch extends Component {
 
   handleResultSelect = (e, { result }) => {
     this.setState({ results: [], isLoading: false, value: '' })
-    this.props.history.push(`/games/${result.slug}`, result)
+    this.props.history.push(`/games/${result.slug}`, result.id)
   };
 
   search = debounce(value => {
@@ -64,8 +64,7 @@ class GameSearch extends Component {
             resultRenderer={resultRenderer}
             placeholder={"Search"}
             value={value}
-            noResultsMessage={"No games found."}
-            {...this.props}
+            noResultsMessage={"No games found"}
           />
         </Grid.Column>
       </Grid>
