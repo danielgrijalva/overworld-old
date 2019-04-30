74452d899239d455ab8aa5cdd542e5108eba9a3a
daniel grijalva <jdgs.gt@gmail.com>
2019-04-30 15:32:22 -0700

    :sparkles: better loaders

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -3,51 +3,10 @@ import { Container, Grid } from "semantic-ui-react";
 import Actions from "./actions/Actions";
 import axios from "axios";
 import Moment from "react-moment";
-import "./Game.css";
-import ContentLoader from "react-content-loader";
 import { LazyImage } from "react-lazy-images";
 import Backdrop from "./Backdrop";
-
-const TitleLoader = () => (
-  <ContentLoader
-    height={25}
-    width={400}
-    speed={1}
-    primaryColor="#242b34"
-    secondaryColor="#343d4c"
-  >
-    <rect x="0" y="0" rx="7" ry="7" width="370" height="13" />
-  </ContentLoader>
-);
-
-const ImageLoader = () => (
-  <ContentLoader
-    height={160}
-    width={200}
-    speed={1}
-    primaryColor="#242b34"
-    secondaryColor="#343d4c"
-  >
-    <rect x="0" y="0" rx="4" ry="4" width="200" height="160" />
-  </ContentLoader>
-);
-
-const ListLoader = () => (
-  <ContentLoader
-    height={110}
-    width={400}
-    speed={1}
-    primaryColor="#242b34"
-    secondaryColor="#343d4c"
-  >
-    <rect x="0" y="0" rx="5" ry="5" width="300" height="10" />
-    <rect x="20" y="20" rx="5" ry="5" width="270" height="10" />
-    <rect x="20" y="40" rx="5" ry="5" width="220" height="10" />
-    <rect x="0" y="60" rx="5" ry="5" width="300" height="10" />
-    <rect x="20" y="80" rx="5" ry="5" width="250" height="10" />
-    <rect x="20" y="100" rx="5" ry="5" width="130" height="10" />
-  </ContentLoader>
-);
+import { ImageLoader, TitleLoader, ListLoader } from "./Loaders";
+import "./Game.css";
 
 class Game extends React.Component {
   constructor(props) {
@@ -132,64 +91,66 @@ class Game extends React.Component {
             />
           )}
           <Grid.Row className="game-content">
-            <Grid.Column width={4}>
-              {!isLoading ? (
-                <LazyImage
-                  src={game.image.small_url}
-                  alt="Game cover."
-                  placeholder={({ imageProps, ref }) => (
-                    <img
-                      {...imageProps}
-                      ref={ref}
-                      className="ui rounded image cover placeholder"
-                      src={game.image.thumb_url}
-                    />
-                  )}
-                  actual={({ imageProps }) => (
-                    <img className="ui rounded image cover" {...imageProps} />
-                  )}
-                />
-              ) : (
-                <ImageLoader />
-              )}
-            </Grid.Column>
-            {!isLoading ? (
-              <React.Fragment>
-                <Grid.Column width={8}>
-                  <section className="game-header margin-bottom-sm">
-                    <React.Fragment>
-                      <h1>{game.name}</h1>
-                      <small className="release-date">
-                        <a href="#">
-                          <Moment format="YYYY">
-                            {game.original_release_date}
-                          </Moment>
-                        </a>
-                      </small>
-                    </React.Fragment>
-                  </section>
-                  <section>
-                    <span className="game-info margin-bottom-sm">
-                      <small className="company">
-                        A game by <a href="#">{game.developers[0].name}</a>
-                      </small>
-                    </span>
-                    <span
-                      style={{ textAlign: "justify" }}
-                      dangerouslySetInnerHTML={this.createMarkup(game.deck)}
-                    />
-                  </section>
-                </Grid.Column>
-                <Grid.Column width={4}>
-                  <Actions />
-                </Grid.Column>
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                <TitleLoader />
-                <ListLoader />
-              </React.Fragment>
-            )}
+            <React.Fragment>
+              <Grid.Column width={4}>
+                {!isLoading ? (
+                  <LazyImage
+                    src={game.image.small_url}
+                    alt="Game cover."
+                    placeholder={({ imageProps, ref }) => (
+                      <img
+                        {...imageProps}
+                        ref={ref}
+                        className="ui rounded image cover placeholder"
+                        src={game.image.thumb_url}
+                      />
+                    )}
+                    actual={({ imageProps }) => (
+                      <img className="ui rounded image cover" {...imageProps} />
+                    )}
+                  />
+                ) : (
+                  <ImageLoader />
+                )}
+              </Grid.Column>
+              <Grid.Column width={8}>
+                {!isLoading ? (
+                  <React.Fragment>
+                    <section className="game-header margin-bottom-sm">
+                      <React.Fragment>
+                        <h1>{game.name}</h1>
+                        <small className="release-date">
+                          <a href="#">
+                            <Moment format="YYYY">
+                              {game.original_release_date}
+                            </Moment>
+                          </a>
+                        </small>
+                      </React.Fragment>
+                    </section>
+                    <section>
+                      <span className="game-info margin-bottom-sm">
+                        <small className="company">
+                          A game by <a href="#">{game.developers[0].name}</a>
+                        </small>
+                      </span>
+                      <span
+                        style={{ textAlign: "justify" }}
+                        dangerouslySetInnerHTML={this.createMarkup(game.deck)}
+                      />
+                    </section>
+                  </React.Fragment>
+                ) : (
+                  <React.Fragment>
+                    <TitleLoader />
+                    <ListLoader />
+                  </React.Fragment>
+                )}
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <Actions />
+              </Grid.Column>
+            </React.Fragment>
           </Grid.Row>
         </Grid>
       </Container>
