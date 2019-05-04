c69d529d7f47f91fd27e9cbbec2d6a63c47e190b
daniel grijalva <jdgs.gt@gmail.com>
2019-05-03 23:24:26 -0700

    :alien: more API fields - also, code is now more pythonic

--- backend/api/fields.py
+++ backend/api/fields.py
@@ -1,2 +1,26 @@
-fields = 'deck,description,developers,expected_release_year,genres,guid,id,image,images,image_tags,name,original_release_date'
-search_fields = 'name,guid'
+_fields = [
+    'deck',
+    'description',
+    'developers',
+    'expected_release_year',
+    'genres',
+    'guid',
+    'image',
+    'images',
+    'image_tags',
+    'name',
+    'original_release_date',
+    'people',
+    'platforms',
+    'site_detail_url',
+]
+
+_search_fields = [
+    'expected_release_year',
+    'guid',
+    'name',
+    'original_release_date',
+]
+
+fields = ','.join(_fields)
+search_fields = ','.join(_search_fields)
