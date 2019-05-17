8773828ea57759080f406fab06b5925c1d044e95
Daniel Grijalva <jdgs.gt@gmail.com>
2019-05-17 14:45:17 -0700

    :construction: knox views, serializers and urls

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
+from rest_framework. response import Response
+from knox.models import AuthToken
+from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
 
-from . import models
-from . import serializers
 
-class UserListView(generics.ListCreateAPIView):
-    queryset = models.CustomUser.objects.all()
-    serializer_class = serializers.UserSerializer
+class RegisterView(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        user_data = UserSerializer(user, context=self.get_serializer_context())
+
+        return Response({
+                'user': user_data.data,
+                'token': AuthToken.objects.create(user)[1]
+            })
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        user_data = UserSerializer(user, context=self.get_serializer_context())
+
+        return Response({
+                'user': user_data.data,
+                'token': AuthToken.objects.create(user)[1]
+            })    
