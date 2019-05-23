1b025cb2242ea5e5932a37622eebbd8ef0eb9a69
Daniel Grijalva <jdgs.gt@gmail.com>
2019-05-23 15:38:37 -0700

    :sparkles: loads logged-in user on mount

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,28 +1,35 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Game from "./pages/game/Game";
-import Navbar from "./pages/app/components/navbar/Navbar";
-import Landing from "./pages/landing/Landing";
 import { Provider } from "react-redux";
 import store from "./store";
+import Game from "./pages/game/Game";
+import Navbar from "./pages/app/components/navbar/Navbar";
+import App from "./pages/app/App";
+import { loadUser } from "./actions/auth";
 
-function NotFound() {
+const notFound = () => {
   return <p>Not Found</p>;
-}
+};
 
-function AppRouter() {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Landing} />
-          <Route path="/games/:slug" component={Game} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    </Provider>
-  );
+class AppRouter extends React.Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={App} />
+            <Route path="/games/:slug" component={Game} />
+            <Route component={notFound} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
 export default AppRouter;
