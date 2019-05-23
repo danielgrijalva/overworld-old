5b7ad2ba8d696438994215b4a563d717d433333b
Daniel Grijalva <jdgs.gt@gmail.com>
2019-05-23 15:39:17 -0700

    :sparkles: authentication with redux

--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -1,2 +1,12 @@
 export const GET_POPULAR = "GET_POPULAR";
 export const GET_BACKDROP = "GET_BACKDROP";
+export const LOAD_GAME = "LOAD_GAME";
+export const CLEAR_GAME = "CLEAR_GAME";
+export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import landing from "./landing";
+import game from "./game";
+import auth from "./auth";
 
 export default combineReducers({
-  landing
+  landing,
+  game,
+  auth
 });
