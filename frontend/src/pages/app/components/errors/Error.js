0a9cf49e594014ec2dbd7cbae40ca677896e647a
Daniel Grijalva <jdgs.gt@gmail.com>
2019-05-24 10:29:33 -0700

    :fire: removing close button

--- frontend/src/pages/app/components/errors/Error.js
+++ frontend/src/pages/app/components/errors/Error.js
@@ -2,32 +2,13 @@ import React from "react";
 import { Message } from "semantic-ui-react";
 
 export default class Error extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: true
-    };
-  }
-
-  componentWillReceiveProps(props) {
-    this.setState({ visible: true });
-  }
-
-  handleDismiss = () => {
-    this.setState({ visible: false });
-  };
-
   render() {
     const { message } = this.props;
 
-    if (this.state.visible) {
-      return (
-        <Message error onDismiss={this.handleDismiss} {...this.props}>
-          {message}
-        </Message>
-      );
-    }
-
-    return null;
+    return (
+      <Message error {...this.props}>
+        {message}
+      </Message>
+    );
   }
 }
