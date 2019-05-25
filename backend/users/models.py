98d9bec2bf68a4cbb2c863f8c17eb47ce9a4a065
daniel grijalva <jdgs.gt@gmail.com>
2019-05-25 12:37:27 -0700

    fixup! :card_file_box: fields for actions

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,9 +2,12 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from api.models import Game
 
+
 class CustomUser(AbstractUser):
-    games = models.ManyToManyField(Game)
+    played = models.ManyToManyField(Game, related_name='played')
+    liked = models.ManyToManyField(Game, related_name='liked')
+    backlog = models.ManyToManyField(Game, related_name='backlog')
+    wishlist = models.ManyToManyField(Game, related_name='wishlist')
 
     def __str__(self):
         return self.username
-        
