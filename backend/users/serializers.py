4120abd7f19ab03a282f193efc21a21726b7a67d
daniel grijalva <jdgs.gt@gmail.com>
2019-05-25 12:34:27 -0700

    :ambulance: fix serializer field

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -6,7 +6,7 @@ from .models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'games')
+        fields = ('id', 'username', 'email')
 
 
 class RegisterSerializer(serializers.ModelSerializer):
