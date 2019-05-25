e128f38ab6b1449fa7106beef099c124a54c59ac
daniel grijalva <jdgs.gt@gmail.com>
2019-05-25 12:39:22 -0700

    :sparkles: endpoint for user interaction

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,7 +6,15 @@ from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('games/<str:guid>/', views.get_game),
-    path('log/', views.log),
+    path('actions/', views.Actions.as_view()),
+    path('actions/log/', views.Log.as_view()),
+    path('actions/unlog/', views.Unlog.as_view()),
+    path('actions/like/', views.Like.as_view()),
+    path('actions/unlike/', views.Unlike.as_view()),
+    path('actions/wishlist/', views.Wishlist.as_view()),
+    path('actions/remove-wishlist/', views.RemoveWishlist.as_view()),
+    path('actions/backlog/', views.Backlog.as_view()),
+    path('actions/remove-backlog/', views.RemoveBacklog.as_view()),
     path('search/<str:name>', views.search_game),
     path('screenshots/<str:guid>', views.get_screenshots),
     path('games/country/<str:publisher_id>', views.get_game_country),
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,15 @@
 import requests
 from django.conf import settings
-from rest_framework import status
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status, generics, permissions
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from users.serializers import UserSerializer
+from users.models import CustomUser
 from .serializers import GameSerializer
 from .fields import fields, search_fields
+from .models import Game
 
 
 @api_view()
@@ -15,16 +20,6 @@ def get_game(request, guid):
     r = requests.get(url=url, params=params, headers=headers)
 
     return Response(r.json())
-
-
-@api_view(['GET', 'POST'])
-def log(request):
-    if request.method == 'POST':
-        serializer = GameSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
@@ -74,3 +69,128 @@ def get_igdb_cover(request, cover_id):
     r = requests.post(url=url, data=data, headers=headers)
 
     return Response(r.json())
+
+
+class Actions(generics.GenericAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        try:
+            gb = request.GET['gb']
+            name = request.GET['name']
+            game = Game.objects.get(gb=gb, name=name)
+        except ObjectDoesNotExist:
+            return Response([])
+
+        user = CustomUser.objects.get(id=request.user.id)
+        actions = {}
+        actions['played'] = True if game in user.played.all() else False
+        actions['liked'] = True if game in user.liked.all() else False
+        actions['backlog'] = True if game in user.backlog.all() else False
+        actions['wishlist'] = True if game in user.wishlist.all() else False
+
+        return Response(actions)
+
+
+class Log(generics.GenericAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        game, created = Game.objects.get_or_create(**request.data)
+        user = CustomUser.objects.get(id=request.user.id)
+
+        user.played.add(game)
+        serializer = GameSerializer(game).data
+
+        # if the game you just logged was in your backlog, remove it
+        if game in user.backlog.all():
+            user.backlog.remove(game)
+            serializer['removedFromBacklog'] = True
+    
+        print(serializer)
+
+        return Response(serializer)
+
+
+class Unlog(generics.GenericAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        game = Game.objects.get(**request.data)
+        user = CustomUser.objects.get(id=request.user.id)
+        user.played.remove(game)
+        serializer = GameSerializer(game).data
+
+        return Response(serializer)
+
+
+class Like(generics.GenericAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        game, created = Game.objects.get_or_create(**request.data)
+        user = CustomUser.objects.get(id=request.user.id)
+        user.liked.add(game)
+        serializer = GameSerializer(game).data
+
+        return Response(serializer)
+
+
+class Unlike(generics.GenericAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        game = Game.objects.get(**request.data)
+        user = CustomUser.objects.get(id=request.user.id)
+        user.liked.remove(game)
+        serializer = GameSerializer(game).data
+
+        return Response(serializer)
+
+
+class Backlog(generics.GenericAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        game, created = Game.objects.get_or_create(**request.data)
+        user = CustomUser.objects.get(id=request.user.id)
+        user.backlog.add(game)
+        serializer = GameSerializer(game).data
+
+        return Response(serializer)
+
+
+class RemoveBacklog(generics.GenericAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        game = Game.objects.get(**request.data)
+        user = CustomUser.objects.get(id=request.user.id)
+        user.backlog.remove(game)
+        serializer = GameSerializer(game).data
+
+        return Response(serializer)
+
+
+class Wishlist(generics.GenericAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        game, created = Game.objects.get_or_create(**request.data)
+        user = CustomUser.objects.get(id=request.user.id)
+        user.wishlist.add(game)
+        serializer = GameSerializer(game).data
+
+        return Response(serializer)
+
+
+class RemoveWishlist(generics.GenericAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        game = Game.objects.get(**request.data)
+        user = CustomUser.objects.get(id=request.user.id)
+        user.wishlist.remove(game)
+        serializer = GameSerializer(game).data
+
+        return Response(serializer)
