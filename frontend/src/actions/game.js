c44e8691d339ffcc77918b3fe270bcfd91147378
daniel grijalva <jdgs.gt@gmail.com>
2019-05-25 12:42:50 -0700

    :sparkles: redux stuff for actions - stores whether the user has played, liked, backlogged or wishlisted a game

--- frontend/src/actions/game.js
+++ frontend/src/actions/game.js
@@ -1,27 +1,207 @@
 import axios from "axios";
-import { LOAD_GAME, CLEAR_GAME } from "./types";
+import {
+  LOG_GAME,
+  UNLOG_GAME,
+  LIKE_GAME,
+  UNLIKE_GAME,
+  LOAD_ACTIONS,
+  ADD_TO_BACKLOG,
+  REMOVE_FROM_BACKLOG,
+  ADD_TO_WISHLIST,
+  REMOVE_FROM_WISHLIST
+} from "./types";
+import { tokenConfig } from "./auth";
 
-export const loadGame = gameId => dispatch => {
-  dispatch({
-    type: CLEAR_GAME
-  });
+export const loadActions = (gameId, name) => (dispatch, getState) => {
   axios
-    .all([
-      axios.get(`/api/games/${gameId}`),
-      axios.get(`/api/screenshots/${gameId}`)
-    ])
-    .then(
-      axios.spread((game, scr) => {
-        game.data.results.screenshots = scr.data.results;
+    .get(`/api/actions/`, {
+      params: {
+        gb: gameId,
+        name: name
+      },
+      headers: tokenConfig(getState).headers
+    })
+    .then(res => {
+      dispatch({
+        type: LOAD_ACTIONS,
+        payload: res.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
 
+export const logGame = (gameId, name) => (dispatch, getState) => {
+  axios
+    .post(
+      "/api/actions/log/",
+      {
+        gb: gameId,
+        name: name
+      },
+      tokenConfig(getState)
+    )
+    .then(res => {
+      if (res.data.hasOwnProperty("removedFromBacklog")) {
         dispatch({
-          type: LOAD_GAME,
-          gameId: gameId,
-          payload: game.data.results
+          type: REMOVE_FROM_BACKLOG,
+          payload: res.data
         });
+      }
+      dispatch({
+        type: LOG_GAME,
+        payload: res.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
 
-        //   this.getCountry(game.data.results.developers[0].id);
-        //   this.getCountry(game.data.results.publishers[0].id);
-      })
-    );
+export const unlogGame = (gameId, name) => (dispatch, getState) => {
+  axios
+    .post(
+      "/api/actions/unlog/",
+      {
+        gb: gameId,
+        name: name
+      },
+      tokenConfig(getState)
+    )
+    .then(res => {
+      dispatch({
+        type: UNLOG_GAME,
+        payload: res.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+export const likeGame = (gameId, name) => (dispatch, getState) => {
+  axios
+    .post(
+      "/api/actions/like/",
+      {
+        gb: gameId,
+        name: name
+      },
+      tokenConfig(getState)
+    )
+    .then(res => {
+      dispatch({
+        type: LIKE_GAME,
+        payload: res.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+export const unlikeGame = (gameId, name) => (dispatch, getState) => {
+  axios
+    .post(
+      "/api/actions/unlike/",
+      {
+        gb: gameId,
+        name: name
+      },
+      tokenConfig(getState)
+    )
+    .then(res => {
+      dispatch({
+        type: UNLIKE_GAME,
+        payload: res.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+export const addToBacklog = (gameId, name) => (dispatch, getState) => {
+  axios
+    .post(
+      "/api/actions/backlog/",
+      {
+        gb: gameId,
+        name: name
+      },
+      tokenConfig(getState)
+    )
+    .then(res => {
+      dispatch({
+        type: ADD_TO_BACKLOG,
+        payload: res.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+export const removeFromBacklog = (gameId, name) => (dispatch, getState) => {
+  axios
+    .post(
+      "/api/actions/remove-backlog/",
+      {
+        gb: gameId,
+        name: name
+      },
+      tokenConfig(getState)
+    )
+    .then(res => {
+      dispatch({
+        type: REMOVE_FROM_BACKLOG,
+        payload: res.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+export const addToWishlist = (gameId, name) => (dispatch, getState) => {
+  axios
+    .post(
+      "/api/actions/wishlist/",
+      {
+        gb: gameId,
+        name: name
+      },
+      tokenConfig(getState)
+    )
+    .then(res => {
+      dispatch({
+        type: ADD_TO_WISHLIST,
+        payload: res.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+export const removeFromWishlist = (gameId, name) => (dispatch, getState) => {
+  axios
+    .post(
+      "/api/actions/remove-wishlist/",
+      {
+        gb: gameId,
+        name: name
+      },
+      tokenConfig(getState)
+    )
+    .then(res => {
+      dispatch({
+        type: REMOVE_FROM_WISHLIST,
+        payload: res.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 };
--- frontend/src/reducers/game.js
+++ frontend/src/reducers/game.js
@@ -1,23 +1,90 @@
-import { LOAD_GAME, CLEAR_GAME } from "../actions/types";
+import {
+  LOG_GAME,
+  UNLOG_GAME,
+  LOAD_ACTIONS,
+  LIKE_GAME,
+  UNLIKE_GAME,
+  ADD_TO_BACKLOG,
+  REMOVE_FROM_BACKLOG,
+  ADD_TO_WISHLIST,
+  REMOVE_FROM_WISHLIST
+} from "../actions/types";
 
 const initialState = {
-  gameId: "",
-  game: {},
-  countries: ["WILD WILD"],
-  isLoading: true
+  actions: {}
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    case LOAD_GAME:
+    case LOAD_ACTIONS:
       return {
         ...state,
-        gameId: action.gameId,
-        game: action.payload,
-        isLoading: false
+        actions: action.payload
       };
-    case CLEAR_GAME:
-      return state;
+    case LOG_GAME:
+      return {
+        ...state,
+        actions: {
+          ...state.actions,
+          played: true
+        }
+      };
+    case UNLOG_GAME:
+      return {
+        ...state,
+        actions: {
+          ...state.actions,
+          played: false
+        }
+      };
+    case LIKE_GAME:
+      return {
+        ...state,
+        actions: {
+          ...state.actions,
+          liked: true
+        }
+      };
+    case UNLIKE_GAME:
+      return {
+        ...state,
+        actions: {
+          ...state.actions,
+          liked: false
+        }
+      };
+    case ADD_TO_BACKLOG:
+      return {
+        ...state,
+        actions: {
+          ...state.actions,
+          backlog: true
+        }
+      };
+    case REMOVE_FROM_BACKLOG:
+      return {
+        ...state,
+        actions: {
+          ...state.actions,
+          backlog: false
+        }
+      };
+    case ADD_TO_WISHLIST:
+      return {
+        ...state,
+        actions: {
+          ...state.actions,
+          wishlist: true
+        }
+      };
+    case REMOVE_FROM_WISHLIST:
+      return {
+        ...state,
+        actions: {
+          ...state.actions,
+          wishlist: false
+        }
+      };
     default:
       return state;
   }
