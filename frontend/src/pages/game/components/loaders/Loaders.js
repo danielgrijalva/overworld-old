de6868ac0f971f6d31b0de4bdac3dd30af79212b
daniel grijalva <jdgs.gt@gmail.com>
2019-05-25 21:55:02 -0700

    :lipstick: improve, polished loaders

--- frontend/src/pages/game/components/loaders/Loaders.js
+++ frontend/src/pages/game/components/loaders/Loaders.js
@@ -9,35 +9,46 @@ export const TitleLoader = () => (
     primaryColor="#242b34"
     secondaryColor="#343d4c"
   >
-    <rect x="0" y="0" rx="5" ry="5" width="260" height="10" />
+    <rect x="0" y="3" rx="7" ry="7" width="245" height="12" />
   </ContentLoader>
 );
 
 export const ImageLoader = () => (
   <ContentLoader
-    height={160}
+    height={400}
     width={200}
     speed={1}
     primaryColor="#242b34"
     secondaryColor="#343d4c"
   >
-    <rect x="0" y="0" rx="4" ry="4" width="185" height="153" />
+    <rect x="0" y="0" rx="4" ry="4" width="200" height="240" />
+  </ContentLoader>
+);
+
+export const ActionsLoader = () => (
+  <ContentLoader
+    height={203}
+    width={200}
+    speed={1}
+    primaryColor="#242b34"
+    secondaryColor="#343d4c"
+    style={{ marginLeft: "3.1rem" }}
+  >
+    <rect x="0" y="0" rx="4" ry="4" width="200" height="203" />
   </ContentLoader>
 );
 
 export const TextLoader = () => (
   <ContentLoader
-    height={110}
+    height={150}
     width={400}
     speed={1}
     primaryColor="#242b34"
     secondaryColor="#343d4c"
   >
-    <rect x="0" y="0" rx="5" ry="5" width="330" height="10" />
-    <rect x="20" y="20" rx="5" ry="5" width="300" height="10" />
-    <rect x="20" y="40" rx="5" ry="5" width="250" height="10" />
-    <rect x="0" y="60" rx="5" ry="5" width="330" height="10" />
-    <rect x="20" y="80" rx="5" ry="5" width="280" height="10" />
-    <rect x="20" y="100" rx="5" ry="5" width="160" height="10" />
+    <rect x="0" y="5" rx="5" ry="5" width="399" height="10" />
+    <rect x="0" y="25" rx="5" ry="5" width="399" height="10" />
+    <rect x="0" y="45" rx="5" ry="5" width="399" height="10" />
+    <rect x="0" y="65" rx="5" ry="5" width="340" height="10" />
   </ContentLoader>
 );
