2810ce16e5fe28fb76d8f42c425ed4d144c6b840
daniel grijalva <jdgs.gt@gmail.com>
2019-05-25 22:00:05 -0700

    :card_file_box: ratings model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,23 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Game(models.Model):
     gb = models.CharField(max_length=16)
     name = models.CharField(max_length=255, default='default')
-    
+
+    def __str__(self):
+        return '%s - %s' % (self.gb, self.name)
+
+
+class Ratings(models.Model):
+    game = models.ForeignKey('Game', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    rating = models.IntegerField(null=True,
+                                 validators=[MinValueValidator(0), MaxValueValidator(10)])
+
+    def __str__(self):
+        return '%s - %s - %s' % (self.user, self.game, self.rating)
+
+    class Meta:
+        unique_together = ['game', 'user']
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,15 @@
 from .models import Game
 from rest_framework import serializers
-from .models import Game
+from .models import Game, Ratings
 
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = ('gb', 'name',)
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ratings
+        fields = ('game', 'user', 'rating')
