4e510c60a8881e6d455029b0cf503bc36d885da1
Daniel Grijalva <jdgs.gt@gmail.com>
2019-06-19 16:16:41 -0700

    :white_check_mark: unit testing endpoints

---
+++ backend/actions/tests.py
@@ -0,0 +1,96 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from users.models import CustomUser
+from games.models import Game
+
+class ActionsTests(APITestCase):
+    def setUp(self):
+        user_data = {
+            'username': 'testing',
+            'email': 'testing@overworld.com',
+            'password': 'testingOverworld'
+        }
+        self.user = CustomUser.objects.create(**user_data)
+        self.game = Game.objects.create(igdb=1074, name='Super Mario 64')
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_actions(self):
+        """Ensure we can retrieve the actions between a user and a game."""
+        url = reverse('get-actions')
+        data = {'igdb': self.game.igdb, 'name': self.game.name}
+        response = self.client.get(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_log_unlog_game(self):
+        """Ensure we can log and unlog a game."""
+        url = reverse('log-game')
+        data = {'igdb': self.game.igdb, 'name': self.game.name}
+
+        log = self.client.post(url, data, format='json')
+        self.assertEqual(True, log.data['value'])
+
+        unlog = self.client.post(url, data, format='json')
+        self.assertEqual(False, unlog.data['value'])
+
+    def test_like_unlike_game(self):
+        """Ensure we can like and unlike a game."""
+        url = reverse('like-game')
+        data = {'igdb': self.game.igdb, 'name': self.game.name}
+
+        like = self.client.post(url, data, format='json')
+        self.assertEqual(True, like.data['value'])
+
+        unlike = self.client.post(url, data, format='json')
+        self.assertEqual(False, unlike.data['value'])
+
+    def test_add_remove_from_backlog(self):
+        """Ensure we can add and remove a game from our backlog."""
+        url = reverse('add-to-backlog')
+        data = {'igdb': self.game.igdb, 'name': self.game.name}
+
+        add = self.client.post(url, data, format='json')
+        self.assertEqual(True, add.data['value'])
+
+        remove = self.client.post(url, data, format='json')
+        self.assertEqual(False, remove.data['value'])
+
+    def test_add_remove_from_wishlist(self):
+        """Ensure we can add and remove a game from our wishlist."""
+        url = reverse('add-to-wishlist')
+        data = {'igdb': self.game.igdb, 'name': self.game.name}
+
+        add = self.client.post(url, data, format='json')
+        self.assertEqual(True, add.data['value'])
+
+        remove = self.client.post(url, data, format='json')
+        self.assertEqual(False, remove.data['value'])
+
+    def test_get_rating(self):
+        """Ensure we can retrieve the rating."""
+        url = reverse('rate-game')
+        data = {'game': self.game.igdb}
+        response = self.client.get(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_rate_game(self):
+        """Ensure we can rate a game."""
+        url = reverse('rate-game')
+        data = {'game': self.game.igdb, 'rating': 10}
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_invalid_rating_value(self):
+        """Ensure rating values are between 1 and 10."""
+        url = reverse('rate-game')
+        negative_rating = {'game': self.game.igdb, 'rating': -1}
+        big_rating = {'game': self.game.igdb, 'rating': 11}
+        negative = self.client.post(url, negative_rating, format='json')
+        big = self.client.post(url, big_rating, format='json')
+
+        self.assertEqual(negative.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(big.status_code, status.HTTP_400_BAD_REQUEST)
+    
