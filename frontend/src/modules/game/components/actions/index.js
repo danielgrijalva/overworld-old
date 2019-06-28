e901f2aaf2a904b0d94e9163fef578eb6546c87d
Daniel Grijalva <jdgs.gt@gmail.com>
2019-06-28 15:15:49 -0700

    :art: refactored according to api changes - now sending the slug field as well

--- frontend/src/modules/game/components/actions/index.js
+++ frontend/src/modules/game/components/actions/index.js
@@ -1,50 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {
-  Menu,
-  Modal,
-  Header,
-  Form,
-  Button,
-  Checkbox,
-  FormField
-} from "semantic-ui-react";
-import "react-dates/initialize";
-import "react-dates/lib/css/_datepicker.css";
-import moment from "moment";
-import { SingleDatePicker } from "react-dates";
+import { Menu } from "semantic-ui-react";
 import { connect } from "react-redux";
-import Moment from "react-moment";
+import { LogIn } from "../../../app/components";
+import LogModal from "../log-modal";
 import Buttons from "./Buttons";
 import Ratings from "./Rating";
-import { LogIn } from "../../../app/components";
 import "./styles.css";
 
 class Actions extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isModalActive: false,
-      showDate: false,
-      date: moment(),
-      focused: false
-    };
-  }
-
-  closeModal = () => {
-    this.setState({ isModalActive: false });
-  };
-
   handleChange = (event, { name, value }) => {
     if (this.state.hasOwnProperty(name)) {
       this.setState({ [name]: value });
     }
   };
-
-  openModal = () => this.setState({ isModalActive: true });
-
-  showDate = () =>
-    this.setState(prevState => ({ showDate: !prevState.showDate }));
 
   render() {
     return (
@@ -56,70 +25,9 @@ class Actions extends React.Component {
             </Menu.Item>
             <Menu.Item className="rate">
               Rate
-              <Ratings game={this.props.game.id} />
+              <Ratings game={this.props.game} />
             </Menu.Item>
-            <Menu.Item link>
-              <Modal
-                size="mini"
-                open={true}
-                onClose={this.closeModal}
-                trigger={
-                  <Menu.Item
-                    content="Review or log"
-                    onClick={this.openModal}
-                    link
-                  />
-                }
-                closeIcon
-                className="register"
-              >
-                <Modal.Content>
-                  <Modal.Description>
-                    <Header>I played...</Header>
-                    <section className="game-header margin-bottom-sm">
-                      <h2>{this.props.game.name}</h2>
-                      <small className="release-date">
-                        <Moment format="YYYY">
-                          {this.props.game.first_release_date * 1000}
-                        </Moment>
-                      </small>
-                    </section>
-                    <section>
-                      <Form>
-                        <Form.Field>
-                          <label>Specify the day you played it</label>
-                          <SingleDatePicker
-                            id="date"
-                            numberOfMonths={1}
-                            displayFormat="DD MMM YYYY"
-                            date={this.state.date}
-                            focused={this.state.focused}
-                            noBorder={true}
-                            readOnly={true}
-                            hideKeyboardShortcutsPanel={true}
-                            onDateChange={date => this.setState({ date: date })}
-                            onFocusChange={({ focused }) =>
-                              this.setState({ focused })
-                            }
-                          />
-                        </Form.Field>
-                        <Form.Field>
-                          <label>Review</label>
-                          <textarea name="review" />
-                        </Form.Field>
-                        <FormField>
-                          <label>Rating</label>
-                          <Ratings />
-                        </FormField>
-                        <FormField>
-                          <Checkbox label="Contains spoilers" />
-                        </FormField>
-                      </Form>
-                    </section>
-                  </Modal.Description>
-                </Modal.Content>
-              </Modal>
-            </Menu.Item>
+            <LogModal game={this.props.game} />
             <Menu.Item content="Add to a list" link />
           </React.Fragment>
         ) : (
