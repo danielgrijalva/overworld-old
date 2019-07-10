8be5822dfec0e508fa88948b8dce067d0fdb4f13
Daniel Grijalva <jdgs.gt@gmail.com>
2019-07-10 15:15:04 -0700

    :sparkles: choose favorite games component

---
+++ frontend/src/modules/settings/components/choose-favorites/index.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { Modal, Form, Button, Header, Image } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { GameSearch } from "../../../app/components";
+import { addFavorite, removeFavorite } from "../../actions";
+import "./styles.css";
+
+class ChooseFavorites extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      open: false
+    };
+  }
+
+  handleRemove = game => {
+    this.props.removeFavorite(game);
+  };
+
+  handleResultSelect = result => {
+    this.props.addFavorite(result);
+    this.setState({ open: false });
+  };
+
+  handleOpen = () => this.setState({ open: true });
+
+  handleClose = () => this.setState({ open: false });
+
+  render() {
+    const { favorites } = this.props;
+    if (favorites) {
+      return (
+        <Form>
+          <Form.Field>
+            <label>Favorite Games</label>
+          </Form.Field>
+          <div className="choose-favorites-wrapper">
+            {favorites.map((g, i) => {
+              return (
+                <div className="favorite-game-wrapper" key={i}>
+                  <Image
+                    rounded
+                    src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${
+                      g.cover_id
+                    }.jpg`}
+                    className="game-cover"
+                  />
+
+                  <div key={i} className="favorite-overlay">
+                    <Button
+                      onClick={() => this.handleRemove(g)}
+                      circular
+                      className="remove-favorite"
+                      icon="remove"
+                    />
+                  </div>
+                </div>
+              );
+            })}
+            {[...Array(5 - favorites.length)].map((_, i) => (
+              <div key={i} className="placeholder">
+                <Modal
+                  size="mini"
+                  open={this.state.open}
+                  onClose={this.handleClose}
+                  closeIcon
+                  className="register pick-favorites-modal"
+                  trigger={
+                    <Button
+                      onClick={this.handleOpen}
+                      circular
+                      className="add-favorite"
+                      icon="plus"
+                    />
+                  }
+                >
+                  <Modal.Content>
+                    <Modal.Description>
+                      <Header>Add a favorite game</Header>
+                    </Modal.Description>
+                    <GameSearch onResultSelect={this.handleResultSelect} />
+                  </Modal.Content>
+                </Modal>
+              </div>
+            ))}
+          </div>
+        </Form>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
+export default connect(
+  null,
+  { addFavorite, removeFavorite }
+)(ChooseFavorites);
