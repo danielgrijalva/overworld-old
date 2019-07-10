da51f0fc01cfbe4eaf5358c307360369430428fb
Daniel Grijalva <jdgs.gt@gmail.com>
2019-07-10 14:53:06 -0700

    :alien: include favorites in user serializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -37,6 +37,7 @@ class ProfileSerializer(serializers.ModelSerializer):
             'liked',
             'backlog',
             'wishlist',
+            'favorites',
             'following',
             'followers',
         )
