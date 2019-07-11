d9ce219ef1be15416e4654fec0fada8f17843a56
Ben NF <erogone775@gmail.com>
2019-07-11 10:20:57 -0400

    fix getting of home path on windows in backend/backend/settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,7 +19,7 @@ load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-HEROKU = '/app' in os.environ['HOME']
+HEROKU = '/app' in os.path.expanduser('~')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
