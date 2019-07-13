0ca0532b01c892fe4514e7532dcc598b1a1705b7
daniel grijalva <jdgs.gt@gmail.com>
2019-07-13 14:57:31 -0700

    :lipstick: added hover effect on list previews and game cover

--- frontend/src/modules/app/components/cover/index.js
+++ frontend/src/modules/app/components/cover/index.js
@@ -8,7 +8,7 @@ const Cover = ({ imageId, slug, className, size }) => {
   const cover = `https://images.igdb.com/igdb/image/upload/t_cover_${size}/${imageId}.jpg`;
   return (
     <div className={className}>
-      <Link to={`/games/${slug}`}>
+      <Link to={`/games/${slug}`} className="cover-link">
         <Image src={cover} rounded fluid className="cover" />
       </Link>
     </div>
