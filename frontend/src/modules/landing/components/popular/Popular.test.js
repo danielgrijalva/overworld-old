857c0cbaaa2ee1f8ea8dad173d5e88dd832432d9
Mike Smith <michael.eugene.smith@gmail.com>
2019-07-16 09:24:55 -0700

    :white_check_mark: Adding initial Popular component unit tests

---
+++ frontend/src/modules/landing/components/popular/Popular.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Popular from '../popular';
+
+describe('Test <Popular />', () => {
+
+  it('shows required props warning when no props passed', () => {
+    console.error = jest.fn();
+    expect(() => {
+      shallow(<Popular />);
+    }).toThrow();
+
+    expect(console.error).toHaveBeenCalledTimes(2);
+    const isLoadingWarning = console.error.mock.calls[0];
+    const popularWarning = console.error.mock.calls[1];
+    expect(isLoadingWarning[0]).toMatch("The prop `isLoading` is marked as required in `Popular`,");
+    expect(popularWarning[0]).toMatch("The prop `popular` is marked as required in `Popular`,");
+    console.error.mockClear();
+  });
+
+});
+
+describe('Test <Popular /> when isLoading is true', () => {
+
+  let wrap;
+  const props = {
+    isLoading: true,
+    popular: [],
+  }
+
+  beforeAll(() => {
+    wrap = shallow(<Popular {...props}/>);
+  });
+
+  it('renders a section with popular margin-top-xs margin-bottom classes', () => {
+    const section = wrap.find('section');
+    expect(section.hasClass('popular margin-top-xs margin-bottom')).toEqual(true);
+  });
+
+  it('renders 6 placeholders', () => {
+    const placeholders = wrap.find('div');
+    expect(placeholders).toHaveLength(6);
+    placeholders.forEach((node) => {
+      expect(node.hasClass('placeholder')).toEqual(true);
+    });
+  });
+});
