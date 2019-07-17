52b2105bf011729f451642e0835047d6fe3aac43
Nichols-Farquhar <Ben.Nichols-Farquhar@pega.com>
2019-07-17 17:56:51 -0400

    add two tests for edge cases on games api

--- backend/games/tests.py
+++ backend/games/tests.py
@@ -7,6 +7,27 @@ from .fields import game_fields, search_fields, popular_fields, backdrop_fields
 
 class GameTests(APITestCase):
     @patch('games.views.requests.post')
+    def test_get_game_multi_not_found(self, mock_post):
+        """Ensure that the API returns 404 status with an unknown games."""
+        mock_response = Mock()
+        mock_response.json.return_value = []
+        mock_post.return_value = mock_response
+
+        url = reverse('get-games')
+        response = self.client.get(url, {"slugs": "not a game,more not a game"}, format='json')
+
+        # Test mock is called with correct arguments
+        self.assertEqual(mock_post.call_count, 1)
+        self.assertEqual(mock_post.call_args[1]['data'],
+                         f'fields {game_fields}; where slug=("not a game","more not a game");')
+        self.assertEqual(mock_post.call_args[1]['url'],
+                         'https://api-v3.igdb.com/games/')
+        # No game exists with slug = "0", IGDB returns empty []
+        # Test response is 404 with "Game not found." message
+        self.assertEqual(response.json(), {'detail': 'Game not found.'})
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    @patch('games.views.requests.post')
     def test_get_game_multi(self, mock_post):
         """Ensure we can obtain multiple games from IGDB."""
 
@@ -130,8 +151,84 @@ class GameTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     @patch('games.views.requests.post')
-    def test_get_popular(self, mock_post):
-        """Ensure we can obtain a list of popular games."""
+    def test_get_popular_with_limit(self, mock_post):
+        """Ensure we can obtain a list of popular games with invalid limit."""
+        # Create Mock post and response return value
+        mock_response = Mock()
+        expected_dict = [
+            {
+                "id": 1877,
+                "cover": {
+                    "id": 75012,
+                    "image_id": "co1lvo"
+                },
+                "name": "Cyberpunk 2077",
+                "popularity": 1735.401818908951
+            },
+            {
+                "id": 119207,
+                "cover": {
+                    "id": 74840,
+                    "image_id": "co1lqw"
+                },
+                "name": "Aquapark.io",
+                "popularity": 1380.091049131278
+            },
+            {
+                "id": 38967,
+                "cover": {
+                    "id": 75151,
+                    "image_id": "co1lzj"
+                },
+                "name": "Cooking Simulator",
+                "popularity": 1332.181668715655
+            },
+            {
+                "id": 114455,
+                "cover": {
+                    "id": 71287,
+                    "image_id": "co1j07"
+                },
+                "name": "Pacify",
+                "popularity": 536.4335905677425
+            },
+            {
+                "id": 115276,
+                "cover": {
+                    "id": 71673,
+                    "image_id": "co1jax"
+                },
+                "name": "Super Mario Maker 2",
+                "popularity": 339.6555774443441
+            },
+            {
+                "id": 10760,
+                "cover": {
+                    "id": 24758,
+                    "image_id": "jt5ypn4a00wf4bmqrhre"
+                },
+                "name": "Bloodstained: Ritual of the Night",
+                "popularity": 330.4026928147172
+            }]
+        mock_response.json.return_value = expected_dict
+        mock_post.return_value = mock_response
+
+        url = reverse('get-popular')
+        response = self.client.get(url, {"limit": 100}, format='json')
+
+        # Test mock is called with correct arguments
+        self.assertEqual(mock_post.call_count, 1)
+        self.assertEqual(mock_post.call_args[1]['data'],
+                         f'fields {popular_fields}; sort popularity desc; limit 6;')
+        self.assertEqual(mock_post.call_args[1]['url'],
+                         'https://api-v3.igdb.com/games/')
+        # Test response contains expected result
+        self.assertEqual(response.json(), expected_dict)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    @patch('games.views.requests.post')
+    def test_get_popular_without_limit(self, mock_post):
+        """Ensure we can obtain a list of popular games without limit."""
         # Create Mock post and response return value
         mock_response = Mock()
         expected_dict = [
@@ -204,6 +301,7 @@ class GameTests(APITestCase):
         # Test response contains expected result
         self.assertEqual(response.json(), expected_dict)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
 
     @patch('games.views.requests.post')
     def test_get_backdrop(self, mock_post):
