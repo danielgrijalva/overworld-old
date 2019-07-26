73252473ab98c9bd381d3e0d33ecae59ed00c14b
Nichols-Farquhar <Ben.Nichols-Farquhar@pega.com>
2019-07-26 11:17:57 -0400

    change /api/game/muli to /api/game/slugs?

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('popular/', views.get_popular_games, name='get-popular'),
-    path('multi/', views.get_games, name='get-games'),
     path('<str:slug>/', views.get_game, name='get-game'),
     path('search/<str:name>', views.search_game, name='search-game'),
-    path('backdrop/<int:guid>/', views.get_backdrop, name='get-backdrop')
+    path('backdrop/<int:guid>/', views.get_backdrop, name='get-backdrop'),
+    path('', views.get_games, name='get-games'),
+
 ]
