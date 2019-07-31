1d55d92050dbd53fec2812b61159fc572b0c0ce5
Benjamin Nichols-Farquhar <erogone775@gmail.com>
2019-07-31 13:25:17 -0400

    :sparkles: game browser (#111)
    * extend get_popular_games endpoing to take limit parameter
    
    * Add games/multi endpoint allowing to a list of games to be requested at once
    
    * Add gamebrowser view, add actions and reducer to get data for multiple games
    
    * fix Details component to handle when game fields are undefined
    
    * Add backdrop to game browser
    
    * Add handling to api/games/popular for when limit is not passed
    
    * add test for multi games api endpoint
    
    * add two tests for edge cases on games api
    
    * add offset param to /games/popular and update associated tests
    
    * add pagination to gamebrowser
    
    * Rework gamebrowser tiles to use semantic ui Card component
    
    * Add filtering by genre  on frontent and stubs for other filters
    
    * add ability to filter by genre on /games/popular endpoint
    
    * update gamebrowser tile with platform icons and inmprove styling of filters
    
    * add test for /games/popular params and filters
    
    * refactor gamebrowser from landing module to its own module
    
    * refactor gametile to use semantic ui Icon instead of fontawesome
    
    * change /api/game/muli to /api/game/slugs?
    
    * update frontend actuion for new api path
    
    * fix bug when filters are blank
    
    * filter adult games out of popular endpoint
    
    * dynamically fetch genres for genre filter
    
    * fix bug with icons, add tooltips, and other small cleanups
    
    * impliment date filter and add stubs for other filters
    
    * fix test and add test for adultContent
    
    * refafactor rendering of filters into new component
    
    * handle case where no games match filters
    
    * remove load more
    
    * refactor and add /games api tests
    
    * merge
    
    * add more tests to make codecov happy, handle error case in get_game_ratings
    
    * add test for get_game_rating for existing game

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
     path('popular/', views.get_popular_games, name='get-popular'),
+    path('genres/', views.get_genres, name='get-genres'),
     path('<str:slug>/', views.get_game, name='get-game'),
     path('<str:slug>/ratings', views.get_game_ratings, name='get-game-ratings'),
     path('search/<str:name>', views.search_game, name='search-game'),
-    path('backdrop/<int:guid>/', views.get_backdrop, name='get-backdrop')
+    path('backdrop/<int:guid>/', views.get_backdrop, name='get-backdrop'),
+    path('', views.get_games, name='get-games'),
+
 ]
