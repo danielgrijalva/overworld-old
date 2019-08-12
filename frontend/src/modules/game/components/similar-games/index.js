966e581d34e6afd6026d1b1c49572d982ad07063
Amrith <arjun.amrith@gmail.com>
2019-08-12 15:02:09 +0530

    :bug: Fixed a bug where a game without cover image was disrupting the UX
     - :fire: added a try catch block to check if all similar games have an associated cover image
    
    Co-authored-by: Amrithnath <arjun.amrith@gmail.com>
    Co-authored-by: arjunamrith <amrith@tydy.it>

--- frontend/src/modules/game/components/similar-games/index.js
+++ frontend/src/modules/game/components/similar-games/index.js
@@ -9,10 +9,17 @@ const Similar = ({similar_games,isLoading})=>{
     return(<div className="similar">
         <Header className="white">Similar Games</Header>
         <section className="sec">
-        {!isLoading ? (
+        {similar_games && !isLoading ? (
             <React.Fragment>
                 {similar_games.map((g,i)=>{
-                    return (<Cover size="big" imageID={g.cover.image_id} slug={g.slug} key={i} id={g} name={g.name}/>)
+                    try {
+                        return (<Cover size="big" imageID={g.cover.image_id} slug={g.slug} key={i} id={g} name={g.name}/>)
+                        
+                    } catch (error) {
+                        [...Array(6)].map((_, i) => (
+                            <div key={i} className="placeholder" />
+                        ))
+                    }
                 })}
             </React.Fragment>
         ):(
