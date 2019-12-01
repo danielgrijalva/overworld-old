aff645d5044380a74a7fb3de129fb52565acaf8d
Aaron Dosser <adosser@ded.zone>
2019-12-01 18:45:47 -0500

    Refactor search and app to use FCs

--- frontend/src/modules/app/index.js
+++ frontend/src/modules/app/index.js
@@ -1,24 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Container } from "semantic-ui-react";
 import Landing from "../landing/";
 
-class App extends Component {
-  render() {
-    if (this.props.auth.isAuthenticated) {
-      return (
-        <Container>
-          <h1>Welcome</h1>
-        </Container>
-      );
-    } else if (this.props.auth.isLoading) {
-      return null;
-    } else {
-      return <Landing />;
-    }
+const App = ({ auth }) => {
+  if (auth.isAuthenticated) {
+    return (
+      <Container>
+        <h1>Welcome</h1>
+      </Container>
+    );
+  } else if (auth.isLoading) {
+    return null;
+  } else {
+    return <Landing />;
   }
-}
+};
 
 App.propTypes = {
   auth: PropTypes.object.isRequired
